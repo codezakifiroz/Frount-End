252c3a76f827dcb88097592a43eb9d69ba63f980
codezakifiroz <firozkhan907979@gmail.com>
2024-01-18 23:14:07 +0530

    secound

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,6 @@
-const countVoval = (str) =>{
-    let count =0;
-    for(const i of str){
-        if(i === 'a'|| i==='i' || i === 'e' || i === 'o' || i === 'r')
-            count++;
-    }
-    return count;
+function squre(value){
 
+    value.forEach((element) => {
+        console.log(element*element);
+    });
 }
---
+++ project3/script.js
@@ -0,0 +1,4 @@
+var red = document.getElementById("red");
+const colorChange = ()=>{
+    document.body.innerHTML = "background-color: red"
+}
